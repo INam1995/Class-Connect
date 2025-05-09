991e03e76800882cfa9a4c20c0dcafa24ce6892a
anjali <anjaliyaduvanshi896242@gmail.com>
2025-05-09 10:36:42 +0530

    addans

--- Frontend/src/Components/DiscussionComponents/AddAnswerPage.jsx
+++ Frontend/src/Components/DiscussionComponents/AddAnswerPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import api from "../../api";
+import api from "../api";
 import { Button, TextField, Typography, Box } from "@mui/material";
 
 export default function AddAnswerPage() {
