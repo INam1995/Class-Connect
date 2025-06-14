89d624a108dfec3468ff0bea25653c6bbe214441
inamyadav <inamyadav23@gmail.com>
2025-06-14 12:16:26 +0530

    code rearrangement done

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { getUserProfile } from '../Controllers/profileController.js'; // Import the controller
-import { updateEmail ,updatePassword} from '../Controllers/settingController.js';
+import { getUserProfile } from '../Controllers/profileController/profileController.js'; // Import the controller
+import { updateEmail ,updatePassword} from '../Controllers/profileController/settingController.js'; // Import the controller for email and password updates
 const router = express.Router();
 
 // Get user profile details
